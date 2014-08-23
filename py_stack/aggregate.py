1a65aa0b51cb82b8abb6c831f43aa31192af60a7
Wojtek Matyskiewicz <wojtek@matyskiewicz.com>
2014-08-23 19:35:51 +0200

    update dicts and lists

--- py_stack/aggregate.py
+++ py_stack/aggregate.py
@@ -8,18 +8,29 @@ from py_stack.stack import Stack
 class Aggregate(Stack):
 
     @classmethod
+    def _replace(cls, field, item, stack_item, replace_fields=None):
+        if not replace_fields and not field in item.keys() or not item[field]:
+            item[field] = stack_item[field]
+        elif type(item.get(field)).__name__ == 'dict':
+            old_value = item[field].copy()
+            for key, value in stack_item[field].items():
+                if not key in old_value.keys():
+                    old_value[key] = value
+            item[field] = old_value
+        elif type(item.get(field)).__name__ == 'list':
+            item[field] = list(
+                set(itertools.chain(item[field], stack_item[field]))
+            )
+        return item
+
+    @classmethod
     def _update_item(cls, key, merged_data, stack_item):
         for item in merged_data:
             if item.get(key) != stack_item.get(key):
                 continue
-
             fields_list = itertools.chain(item.keys(), stack_item.keys())
             for field in fields_list:
-                try:
-                    if not item[field]:
-                        item[field] = stack_item[field]
-                except KeyError:
-                    item[field] = stack_item[field]
+                cls._replace(field, item, stack_item)
             break
         return item
 
@@ -31,7 +42,6 @@ class Aggregate(Stack):
             cls._update_item(key, merged_data, stack_item)
         else:
             merged_data.append(stack_item)
-
         return merged_data
 
     @classmethod
@@ -43,4 +53,6 @@ class Aggregate(Stack):
         while cls.stack:
             stack_item = cls.stack.pop()
             merged_data = cls._add_or_update(key, merged_data, stack_item)
-        return merged_data
+        Stack.set(merged_data)
+        Stack.sort(key='id')
+        return Stack.get()
--- tests/test_aggregate.py
+++ tests/test_aggregate.py
@@ -22,16 +22,15 @@ class TestStack(unittest.TestCase):
             {'id': 1, 'first_name': '',
                 'last_name': '', 'city': 'Los Angeles'},
         ]
-
-        Aggregate.set(data)
-        merged_data = Aggregate.merge(key='id')
-
         valid_data = [
             {'city': 'Los Angeles', 'first_name': 'John',
                 'last_name': 'Schmit', 'id': 1},
             {'city': 'Poznan', 'first_name': 'Arthur',
                 'id': 2, 'last_name': 'Moor'}
         ]
+
+        Aggregate.set(data)
+        merged_data = Aggregate.merge(key='id')
         self.assertTrue(Aggregate.isEmpty())
         self.assertEqual(tuple(merged_data), tuple(valid_data))
 
@@ -57,18 +56,41 @@ class TestStack(unittest.TestCase):
             {'id': 1, 'first_name': '',
                 'last_name': '', 'city': 'Los Angeles'},
         ]
-
-        Aggregate.set(data)
-        merged_data = Aggregate.merge(key='id')
-
         valid_data = [
             {'city': 'Los Angeles', 'first_name': 'John',
                 'last_name': 'Schmit', 'id': 1},
             {'city': 'Poznan', 'first_name': 'Arthur',
                 'id': 2, 'last_name': 'Moor'}
         ]
+
+        Aggregate.set(data)
+        merged_data = Aggregate.merge(key='id')
         self.assertTrue(Aggregate.isEmpty())
         self.assertEqual(tuple(merged_data), tuple(valid_data))
+
+    def test_update_lists_dicts_field(self):
+        ''' Check if dicts, and lists are update '''
+
+        data = [
+            {'id': 1, 'name': '', 'values': [1, 2]},
+            {'id': 1, 'name': 'Numbers', 'values': [1, 3, 4]},
+            {'id': 2, 'name': 'Words', 'values': {'a': 1, 'c': 3}},
+            {'id': 2, 'name': 'Words', 'values': {'a': 2, 'b': 2}},
+        ]
+        valid_data = [
+            {'id': 1, 'name': 'Numbers', 'values': [1, 2, 3, 4]},
+            {'id': 2, 'name': 'Words', 'values': {'a': 2, 'b': 2, 'c': 3}},
+        ]
+
+        Aggregate.set(data)
+        merged_data = Aggregate.merge(key='id')
+        self.assertEqual(
+            merged_data[0].get('values'), valid_data[0].get('values')
+        )
+        self.assertEqual(
+            merged_data[1].get('values'), valid_data[1].get('values')
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
